dfb69b8ed423c244d5d1091823d0f3f2607a58be
Mustafa Bukhari <Mustafa Bukhari@BFX-IMPL-MB>
2023-03-01 21:57:21 -0500

    Add project files.

---
+++ ReadingRoom/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ReadingRoom.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
